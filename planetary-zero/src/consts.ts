e7aaaaa074f36b0d51777f185f44480ab509b971
Diego Hernandez <diegofootball19@gmail.com>
2023-07-12 11:25:51 -0500

    add changes to sitecore item

--- planetary-zero/src/consts.ts
+++ planetary-zero/src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "Diego's webiste";
+export const SITE_DESCRIPTION = "Checkout my projects or look around!";
