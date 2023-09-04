f33af4b6268ba739d5b1cf306b9142bea88bc8d2
Diego Hernandez <diegofootball19@gmail.com>
2023-09-04 16:44:08 -0500

    made updated to layout and css

--- planetary-zero/src/consts.ts
+++ planetary-zero/src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Diego";
+export const SITE_TITLE = "Diego Hernandez";
 export const SITE_DESCRIPTION = "Checkout my projects or look around!";
