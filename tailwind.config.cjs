839a61dc65b7ab3c22caf001459fb82b98bac15b
Diego Hernandez <diegofootball19@gmail.com>
2024-01-05 23:32:15 -0600

    add new font highlight

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx,}"],
   theme: {
-    extend: {},
+    extend: {
+      typography: {
+        DEFAULT: {
+          css: {
+            a: {
+              "&:hover": {
+                "text-shadow": "0 0 5px #ffffff",
+              },
+            },
+          },
+        },
+      },
+    },
   },
   darkMode: "class",
   plugins: [require("@tailwindcss/typography")],
